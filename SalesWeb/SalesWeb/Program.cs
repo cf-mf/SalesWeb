2b5232e89a1bdd49e4844ed6ec27dc38086ec36d
Carlos Freire <carlosfreire700@gmail.com>
2025-12-20 12:12:25 -0300

    feat: Seeding Service

--- SalesWeb/SalesWeb/Program.cs
+++ SalesWeb/SalesWeb/Program.cs
@@ -7,18 +7,29 @@ builder.Services.AddDbContext<SalesWebContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<SeedingService>();  
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var seedingService = scope.ServiceProvider
+            .GetRequiredService<SeedingService>();
+
+        seedingService.Seed();
+    }
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
